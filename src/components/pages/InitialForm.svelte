440003c780fa1f10d83d6dd2e10ebda867ba8d64
Luiz de Oliveira <luizcarlos1405@gmail.com>
2021-10-24 19:02:58 -0300

    Make initial form input bordered

--- src/components/pages/InitialForm.svelte
+++ src/components/pages/InitialForm.svelte
@@ -12,7 +12,12 @@
 <label for="name" class="label flex flex-col space-y-4 items-start">
 	<span class="label-text">First Time? Pick a name.</span>
 	<span class="relative">
-		<input id="name" class="input pr-16" bind:value={name} placeholder="Player Name" />
+		<input
+			id="name"
+			class="input input-bordered pr-16"
+			bind:value={name}
+			placeholder="Player Name"
+		/>
 		<button
 			class="absolute top-0 right-0 rounded-l-none btn btn-primary"
 			on:click={handleGoOnClick}
