f75af467d6a266b85d7591fdc2c98b261d07568b
Luiz de Oliveira <luizcarlos1405@gmail.com>
2021-10-24 19:06:33 -0300

    fix pattern not defaulting correctly to yyy

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -43,7 +43,7 @@
 	let theme = (isClient() && localStorage.getItem('theme')) || '';
 	$: isClient() && localStorage.setItem('theme', theme);
 
-	let cardPattern = $state?.cardPattern || '';
+	$: cardPattern = $state?.cardPattern || 'yyy';
 	$: state?.setCardPattern(cardPattern);
 </script>
 
